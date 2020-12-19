34309aac74ad3b34d4f648aa7c751c8d52c4de2a
jonny <jonnywamsley@github.com>
2020-12-19 12:50:23 -0500

    added mux router for website

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, "<h1>Pokemon cards main page</h1>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, "<h1>MyPokemonCardCollection is a place to track what pokemon cards you have collected</h1>")
+}
+
 func main() {
-	fmt.Println("Pokemon cards rock!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/about", about)
+	http.ListenAndServe(":3000", r)
 }
