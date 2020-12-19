d3a8dbfb43876a33719b6ed8984e00573d65fd5e
jonny <jonnywamsley@github.com>
2020-12-19 15:18:11 -0500

    added a nav bar using bootstrap

--- views/view.go
+++ views/view.go
@@ -15,7 +15,8 @@ func NewView(layout string, files ...string) *View {
 	// all new files will add the footer
 	files = append(files, 
 		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml")
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
