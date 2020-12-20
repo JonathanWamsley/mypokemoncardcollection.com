48324f974503feab0434e742056ed75bc3a35b17
jonny <jonnywamsley@github.com>
2020-12-20 17:36:30 -0500

    added psql connection to main

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 
 	"mypokemoncardcollection.com/controllers"
+	"mypokemoncardcollection.com/models"
 
 	"github.com/gorilla/mux"
 )
 
+// all this is dummy config
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "mypokemoncardcollection_dev"
+  )
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+	
 	staticController := controllers.NewStatic()
 	usersController := controllers.NewUsers()
 	
