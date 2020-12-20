527a0e26e3495cad8b61ef2c89dd94e8a2327468
jonny <jonnywamsley@github.com>
2020-12-20 15:41:42 -0500

    added a userService to interact with user model

---
+++ exp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "fmt"
+
+  "mypokemoncardcollection.com/models"
+
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user     = "postgres"
+  password = "password"
+  dbname   = "mypokemoncardcollection_dev"
+)
+
+func main() {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+  us, err := models.NewUserService(psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer us.Close()
+  us.DestructiveReset()
+
+  // This will error because you DO NOT have a user with
+  // this ID, but we will create one soon.
+  user, err := us.ByID(1)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println(user)
+}
