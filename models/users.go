7c96b953474432e38e1afbd4545f8701bf47eb9d
jonny <jonnywamsley@github.com>
2020-12-20 22:42:25 -0500

    the user create creates a hashed password and clears the raw password when creating a user in the db

--- models/users.go
+++ models/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -65,6 +66,13 @@ func (us *UserService) DestructiveReset() error {
 }
 
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
